f99461aeecc011a0470eb378149e1b4c338fb053
navyas211@gmail.com <navyas211@gmail.com>
2021-09-17 15:53:39 +0530

    [NAVYA]ADDED: Welcome to Line Comparison Computation Program

---
+++ src/com/navya/javaprogram/LineComparison.java
@@ -0,0 +1,11 @@
+package com.navya.javaprogram;
+
+public class LineComparison 
+ {
+	public static void main(String[] args) 
+  {
+	
+	System.out.println("Welcome to The Line Comparison Computation Program");	
+		
+	}
+}
